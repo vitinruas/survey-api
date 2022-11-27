c18bc52ca99417ff9b3b7b872f9cecfddfb5d276
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-11-27 16:43:37 -0300

    test: ensure SignUpController returns 400 if an invalid email is provided

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -1,13 +1,31 @@
 import { SignUpController } from './signup-controller'
-import { IController } from '../../protocols/controller-protocol'
-import { IHttpRequest, IHttpResponse } from '../../protocols/http-protocol'
+import { IHttpRequest, IHttpResponse } from '../../interfaces/protocols/http-protocol'
 import { MissingFieldError } from '../../errors/missing-field-error'
+import { InvalidFieldError } from '../../errors/invalid-field-error'
+import { IEmailValidatorAdapter } from '../../interfaces/dependencies/email-validator-adapter-dependency'
 
-const makeSut = (): SignUpController => new SignUpController()
+interface ISut {
+  sut: SignUpController,
+  emailValidatorStub: IEmailValidatorAdapter
+}
+
+const makeSut = (): ISut => {
+  class EmailValidatorStub {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  const emailValidatorStub: IEmailValidatorAdapter = new EmailValidatorStub()
+  const sut: SignUpController = new SignUpController(emailValidatorStub)
+  return {
+    sut,
+    emailValidatorStub
+  }
+}
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
-    const sut: IController = makeSut()
+    const {sut }: ISut = makeSut()
     const httpRequest: IHttpRequest = {
       body: {
         email: 'any_email@mail.com',
@@ -22,7 +40,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingFieldError('name'))
   })
   test('Should return 400 if no email is provided', () => {
-    const sut: IController = makeSut()
+    const {sut }: ISut = makeSut()
     const httpRequest: IHttpRequest = {
       body: {
         name: 'any_name',
@@ -37,7 +55,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingFieldError('email'))
   })
   test('Should return 400 if no password is provided', () => {
-    const sut: IController = makeSut()
+    const {sut }: ISut = makeSut()
     const httpRequest: IHttpRequest = {
       body: {
         name: 'any_name',
@@ -52,7 +70,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingFieldError('password'))
   })
   test('Should return 400 if no passwordConfirmation is provided', () => {
-    const sut: IController = makeSut()
+    const {sut }: ISut = makeSut()
     const httpRequest: IHttpRequest = {
       body: {
         name: 'any_name',
@@ -65,5 +83,22 @@ describe('SignUp Controller', () => {
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingFieldError('passwordConfirmation'))
+  })
+  test('Should return 400 if an invalid email is provided', () => {
+    const {sut, emailValidatorStub }: ISut = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest: IHttpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    }
+
+    const httpResponse: IHttpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidFieldError('email'))
   })
 })
