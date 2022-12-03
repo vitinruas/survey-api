a1a3a577c65474a7a1a23b1742025d413c2d4fa5
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-12-03 14:27:05 -0300

    test: call Validator.isEmail with correct value

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -17,8 +17,17 @@ describe('EmailValidatorAdapter', () => {
 
   test('Should returns true if a valid email is provided', () => {
     const sut = new EmailValidatorAdapter()
-    const isValid: boolean = sut.isValid('invalid_email@mail.com')
+    const isValid: boolean = sut.isValid('valid_email@mail.com')
 
     expect(isValid).toBe(true)
+  })
+
+  test('Should call Validator.isEmail with correct value', () => {
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+
+    sut.isValid('any_email@mail.com')
+
+    expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
