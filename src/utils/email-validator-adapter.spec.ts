9026aab78ff1bb5e05299eded769fb721656c4c4
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-12-03 14:29:11 -0300

    refactor: move sut creation to a helper function

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -7,23 +7,30 @@ jest.mock('validator', () => ({
   },
 }))
 
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
 describe('EmailValidatorAdapter', () => {
   test('Should returns false if an invalid email is provided', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut: EmailValidatorAdapter = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+
     const isValid: boolean = sut.isValid('invalid_email@mail.com')
+
     expect(isValid).toBe(false)
   })
 
   test('Should returns true if a valid email is provided', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut: EmailValidatorAdapter = makeSut()
+
     const isValid: boolean = sut.isValid('valid_email@mail.com')
 
     expect(isValid).toBe(true)
   })
 
   test('Should call Validator.isEmail with correct value', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut: EmailValidatorAdapter = makeSut()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
 
     sut.isValid('any_email@mail.com')
