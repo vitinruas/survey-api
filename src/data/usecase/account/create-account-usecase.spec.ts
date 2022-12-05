532654492b54a87e7917916db0e68a15a5604cfe
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-12-04 23:24:12 -0300

    refactor: move sut creation to a factory helper method

--- src/data/usecase/account/create-account-usecase.spec.ts
+++ src/data/usecase/account/create-account-usecase.spec.ts
@@ -2,16 +2,33 @@ import { IAddAccountDTO } from '../../../domain/dtos/add-account-dto'
 import { IEncrypterAdapter } from '../../interfaces/dependencies/account/encrypter-adapter'
 import { CreateAccountUseCase } from './create-account-usecase'
 
+const makeEncrypterAdapterStub = (): IEncrypterAdapter => {
+  class EncrypterAdapterStub implements IEncrypterAdapter {
+    async encrypt(value: string): Promise<string> {
+      return Promise.resolve('hashed_password')
+    }
+  }
+  return new EncrypterAdapterStub()
+}
+
+interface ISut {
+  encrypterAdapterStub: IEncrypterAdapter
+  sut: CreateAccountUseCase
+}
+
+const makeSut = (): ISut => {
+  const encrypterAdapterStub: IEncrypterAdapter = makeEncrypterAdapterStub()
+  const sut = new CreateAccountUseCase(encrypterAdapterStub)
+  return {
+    sut,
+    encrypterAdapterStub,
+  }
+}
+
 describe('CreateAccountUseCase', () => {
   test('Should call Encrypter with correct information', async () => {
-    class EncrypterStub implements IEncrypterAdapter {
-      async encrypt(value: string): Promise<string> {
-        return Promise.resolve('hashed_password')
-      }
-    }
-    const encrypterStub = new EncrypterStub()
-    const sut = new CreateAccountUseCase(encrypterStub)
-    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
+    const { sut, encrypterAdapterStub } = makeSut()
+    const encryptSpy = jest.spyOn(encrypterAdapterStub, 'encrypt')
     const accountData: IAddAccountDTO = {
       name: 'valid_name',
       email: 'valid_email@mail.com',
