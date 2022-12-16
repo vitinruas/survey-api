f68b137cfdb5ae9a244f7effe4e938a091cba217
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-12-16 16:49:17 -0300

    feat: ensure CreateAccountUseCase returns an account if all steps succeeds

--- src/data/usecase/account/create-account-usecase.ts
+++ src/data/usecase/account/create-account-usecase.ts
@@ -14,14 +14,9 @@ export class CreateAccountUseCase implements IAddAccountUseCase {
 
   async addAccount(account: IAddAccountDTO): Promise<IAccountEntitie> {
     const password: string = await this.encrypterAdapter.encrypt(account.password)
-    await this.addAccountRepository.addAccount(Object.assign({}, account, { password }))
-    return Promise.resolve({
-      id: 'valid_id',
-      name: 'valid_name',
-      email: 'valid_email@mail.com',
-      password: 'valid_password',
-      created_at: new Date('2001-01-01 00:00'),
-      updated_at: new Date('2001-01-01 00:00'),
-    })
+    const createdAccount = await this.addAccountRepository.addAccount(
+      Object.assign({}, account, { password })
+    )
+    return createdAccount
   }
 }
