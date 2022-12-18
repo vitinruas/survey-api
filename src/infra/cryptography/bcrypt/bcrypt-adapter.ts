b10dae97389fbc770816b2139dcbfef88a163de0
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-12-18 12:45:34 -0300

    feat: ensure BcryptAdapter returns a hash if all steps have been successful

--- src/infra/cryptography/bcrypt/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt/bcrypt-adapter.ts
@@ -4,7 +4,7 @@ import Bcrypt from 'bcrypt'
 export class BcryptAdapter implements IEncrypterAdapter {
   constructor(private readonly salt: number) {}
   async encrypt(value: string): Promise<string> {
-    await Bcrypt.hash(value, this.salt)
-    return Promise.resolve('')
+    const hash = await Bcrypt.hash(value, this.salt)
+    return hash
   }
 }
