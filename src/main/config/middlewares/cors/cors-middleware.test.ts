af143853416f45d675b8da0d9e3198bc93c510d5
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-12-20 12:03:25 -0300

    test: ensure Express parses body requests as json

---
+++ src/main/config/middlewares/cors/cors-middleware.test.ts
@@ -0,0 +1,11 @@
+import request from 'supertest'
+import { app } from '../../app'
+
+describe('CorsMiddleware', () => {
+  test('Should enable cors for external request', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send()
+    })
+    await request(app).post('/test_cors').expect('access-control-allow-origin', '*')
+  })
+})
