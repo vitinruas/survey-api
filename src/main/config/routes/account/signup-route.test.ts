775a1de2fa9b58b9125112c4373362eb5481922a
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-12-20 13:01:23 -0300

    test: connect to mongodb before integration tests

--- src/main/config/routes/account/signup-route.test.ts
+++ src/main/config/routes/account/signup-route.test.ts
@@ -1,7 +1,21 @@
 import request from 'supertest'
+import { MongoDBHelper } from '../../../../infra/repository/mongodb/helpers/mongodb-helper'
 import { app } from '../../app'
 
 describe('SignUpRoute', () => {
+  const mongodb = MongoDBHelper.instance
+  beforeAll(async () => {
+    await mongodb.connect()
+  })
+
+  afterAll(async () => {
+    await mongodb.disconnect()
+  })
+
+  beforeEach(async () => {
+    const collection = await mongodb.getCollection('accounts')
+    await collection.deleteMany({})
+  })
   test('Should return an account whether all steps have been successful', async () => {
     await request(app)
       .post('/api/signup')
