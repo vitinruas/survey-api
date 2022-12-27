2b17dac5a6bad0c1b1eae06654c6337e221e719c
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-12-27 13:37:31 -0300

    refactor: remove duplicated code

--- src/presentation/controllers/account/signup/signup-controller.spec.ts
+++ src/presentation/controllers/account/signup/signup-controller.spec.ts
@@ -1,8 +1,5 @@
-import {
-  MissingFieldError,
-  InvalidFieldError,
-  ServerError,
-} from '../../../errors'
+import { MissingFieldError, InvalidFieldError, ServerError } from '../../../errors'
+import { created, serverError, badRequest } from '../../../helper/http-helper'
 import { SignUpController } from './signup-controller'
 import {
   IHttpRequest,
@@ -22,19 +19,28 @@ const makeEmailValidatorStub = (): IEmailValidatorAdapter => {
   return new EmailValidatorStub()
 }
 
+const makeFakeRequest = (): IHttpRequest => ({
+  body: {
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'any_password',
+    passwordConfirmation: 'any_password',
+  },
+})
+
+const makeFakeCreatedAccount = (): IAccountEntitie => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  password: 'valid_password',
+  created_at: new Date('2001-01-01 00:00'),
+  updated_at: new Date('2001-01-01 00:00'),
+})
+
 const makeAddAccountUseCaseStub = (): IAddAccountUseCase => {
   class AddAccountUseCase implements IAddAccountUseCase {
     async addAccount(addAccountDTO: IAddAccountDTO): Promise<IAccountEntitie> {
-      const createdFakeAccount: IAccountEntitie = {
-        id: 'valid_id',
-        name: 'valid_name',
-        email: 'valid_email@mail.com',
-        password: 'valid_password',
-        created_at: new Date('2001-01-01 00:00'),
-        updated_at: new Date('2001-01-01 00:00'),
-      }
-
-      return Promise.resolve(createdFakeAccount)
+      return Promise.resolve(makeFakeCreatedAccount())
     }
   }
   return new AddAccountUseCase()
@@ -49,10 +55,7 @@ interface ISut {
 const makeSut = (): ISut => {
   const emailValidatorStub: IEmailValidatorAdapter = makeEmailValidatorStub()
   const addAccountUseCaseStub: IAddAccountUseCase = makeAddAccountUseCaseStub()
-  const sut: SignUpController = new SignUpController(
-    emailValidatorStub,
-    addAccountUseCaseStub
-  )
+  const sut: SignUpController = new SignUpController(emailValidatorStub, addAccountUseCaseStub)
   return {
     sut,
     emailValidatorStub,
@@ -74,7 +77,7 @@ describe('SignUp Controller', () => {
     const httpResponse: IHttpResponse = await sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingFieldError('name'))
+    expect(httpResponse).toEqual(badRequest(new MissingFieldError('name')))
   })
 
   test('Should return 400 if no email is provided', async () => {
@@ -90,7 +93,7 @@ describe('SignUp Controller', () => {
     const httpResponse: IHttpResponse = await sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingFieldError('email'))
+    expect(httpResponse).toEqual(badRequest(new MissingFieldError('email')))
   })
 
   test('Should return 400 if no password is provided', async () => {
@@ -106,7 +109,7 @@ describe('SignUp Controller', () => {
     const httpResponse: IHttpResponse = await sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingFieldError('password'))
+    expect(httpResponse).toEqual(badRequest(new MissingFieldError('password')))
   })
 
   test('Should return 400 if no passwordConfirmation is provided', async () => {
@@ -122,9 +125,7 @@ describe('SignUp Controller', () => {
     const httpResponse: IHttpResponse = await sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(
-      new MissingFieldError('passwordConfirmation')
-    )
+    expect(httpResponse).toEqual(badRequest(new MissingFieldError('passwordConfirmation')))
   })
 
   test('Should return 400 if provided passwords do not match', async () => {
@@ -141,24 +142,14 @@ describe('SignUp Controller', () => {
     const httpResponse: IHttpResponse = await sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(
-      new InvalidFieldError('passwordConfirmation')
-    )
+    expect(httpResponse).toEqual(badRequest(new InvalidFieldError('passwordConfirmation')))
   })
 
   test('Should call EmailValidatorAdapter with correct information', async () => {
     const { sut, emailValidatorStub }: ISut = makeSut()
     const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
-    const httpRequest: IHttpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password',
-      },
-    }
 
-    await sut.handle(httpRequest)
+    await sut.handle(makeFakeRequest())
 
     expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
@@ -166,56 +157,30 @@ describe('SignUp Controller', () => {
   test('Should return 400 if an invalid email is provided', async () => {
     const { sut, emailValidatorStub }: ISut = makeSut()
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
-    const httpRequest: IHttpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'invalid_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password',
-      },
-    }
 
-    const httpResponse: IHttpResponse = await sut.handle(httpRequest)
+    const httpResponse: IHttpResponse = await sut.handle(makeFakeRequest())
 
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new InvalidFieldError('email'))
+    expect(httpResponse).toEqual(badRequest(new InvalidFieldError('email')))
   })
 
   test('Should return 500 if EmailValidatorAdapter throws an error', async () => {
     const { sut, emailValidatorStub }: ISut = makeSut()
     // we are changing a mocked class method to throw an error.
-    jest
-      .spyOn(emailValidatorStub, 'isValid')
-      .mockImplementationOnce((): never => {
-        throw new Error()
-      })
-    const httpRequest: IHttpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password',
-      },
-    }
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce((): never => {
+      throw new Error()
+    })
 
-    const httpResponse: IHttpResponse = await sut.handle(httpRequest)
+    const httpResponse: IHttpResponse = await sut.handle(makeFakeRequest())
 
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse).toEqual(serverError(new ServerError('any_stack')))
   })
 
   test('Should call AddAccountUseCase with correct information', async () => {
     const { sut, addAccountUseCaseStub }: ISut = makeSut()
     const addAccountSpy = jest.spyOn(addAccountUseCaseStub, 'addAccount')
-    const httpRequest: IHttpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password',
-      },
-    }
 
-    await sut.handle(httpRequest)
+    await sut.handle(makeFakeRequest())
 
     expect(addAccountSpy).toHaveBeenCalledWith({
       name: 'any_name',
@@ -229,47 +194,17 @@ describe('SignUp Controller', () => {
     // we are changing a mocked class method to throw an error.
     jest
       .spyOn(addAccountUseCaseStub, 'addAccount')
-      .mockImplementationOnce(
-        async (): Promise<never> => Promise.reject(new Error())
-      )
-    const httpRequest: IHttpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password',
-      },
-    }
+      .mockImplementationOnce(async (): Promise<never> => Promise.reject(new Error()))
 
-    const httpResponse: IHttpResponse = await sut.handle(httpRequest)
+    const httpResponse: IHttpResponse = await sut.handle(makeFakeRequest())
 
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse).toEqual(serverError(new ServerError('any_stack')))
   })
 
   test('Should call AddAccountUseCase with correct information', async () => {
     const { sut }: ISut = makeSut()
-    const httpRequest: IHttpRequest = {
-      body: {
-        name: 'valid_name',
-        email: 'valid_email@mail.com',
-        password: 'valid_password',
-        passwordConfirmation: 'valid_password',
-      },
-    }
 
-    const createdAccount: IAccountEntitie = {
-      id: 'valid_id',
-      name: 'valid_name',
-      email: 'valid_email@mail.com',
-      password: 'valid_password',
-      created_at: new Date('2001-01-01 00:00'),
-      updated_at: new Date('2001-01-01 00:00'),
-    }
-
-    const response: IHttpResponse = await sut.handle(httpRequest)
-    expect(response).toEqual({
-      statusCode: 201,
-      body: createdAccount,
-    })
+    const response: IHttpResponse = await sut.handle(makeFakeRequest())
+    expect(response).toEqual(created(makeFakeCreatedAccount()))
   })
 })
