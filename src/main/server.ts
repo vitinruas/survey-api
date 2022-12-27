ecbc4be1a5c3ae030dbc8dc41a545c2f08142b58
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-12-27 15:42:22 -0300

    refactor: change server log when it is started

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,16 +3,18 @@ import { MongoDBHelper } from '../infra/repository/mongodb/helpers/mongodb-helpe
 import { config } from 'dotenv'
 config()
 
-const port = 8000
+const port = process.env.PORT as string
 const mongo = MongoDBHelper.instance
 mongo
   .connect(process.env.MONGO_URL as string)
   .then(async (): Promise<void> => {
+    console.log(`OK -> Connected to DataBase`)
     const app = (await import('./config/app')).app
     app.listen(port, async () => {
-      console.log(`API running at http://localhost:${port}`)
+      console.log(`OK -> API running at http://localhost:${port}`)
+      console.log('CTRL + C to stop running')
     })
   })
   .catch((err) => {
-    console.error('ERROR: Unable to connect to database\n', err)
+    console.error('ERROR -> Unable to connect to database:', err)
   })
