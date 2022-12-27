386de0544ea2319c8802964b8346bf047d4db98b
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-12-27 15:39:35 -0300

    feat: add LogRepository in composition

--- src/main/compositions/account/signup-controller-composition.ts
+++ src/main/compositions/account/signup-controller-composition.ts
@@ -2,6 +2,7 @@ import { CreateAccountUseCase } from '../../../data/usecase/account/create/creat
 import { BcryptAdapter } from '../../../infra/cryptography/bcrypt/bcrypt-adapter'
 import { MongoDBAccountRepository } from '../../../infra/repository/mongodb/account/mongodb-account-repository'
 import { MongoDBHelper } from '../../../infra/repository/mongodb/helpers/mongodb-helper'
+import { LoggerRepository } from '../../../infra/repository/mongodb/logger/logger-repository'
 import { SignUpController } from '../../../presentation/controllers/account/signup/signup-controller'
 import { IController } from '../../../presentation/interfaces'
 import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
@@ -14,7 +15,8 @@ const signUpControllerCompositionFactory = (): IController => {
   const bcryptAdapter = new BcryptAdapter(12)
   const createAccountUseCase = new CreateAccountUseCase(bcryptAdapter, addAccountRepository)
   const signUpController = new SignUpController(emailValidatorAdapter, createAccountUseCase)
-  const logControllerDecorator = new LogControllerDecorator(signUpController)
+  const logRepository = new LoggerRepository()
+  const logControllerDecorator = new LogControllerDecorator(signUpController, logRepository)
   return logControllerDecorator
 }
 
